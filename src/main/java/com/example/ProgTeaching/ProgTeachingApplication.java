0f4367e06e96abb8cf259b41e54d5ef9d9aa2c52
fehervivien <fehervivien96@gmail.com>
2026-09-25 18:56:47 +0200

    Initial project upload

---
+++ src/main/java/com/example/ProgTeaching/ProgTeachingApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgTeaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgTeachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgTeachingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProgTeaching/ProgTeachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProgTeaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgTeachingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
